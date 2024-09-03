9d9d936b969045372287de80d7538b737ffe5762
avict18 <avict1809@gmail.com>
2024-09-03 11:20:48 +0300

    started blog project

--- README.md
+++
@@ -1,2 +0,0 @@
-# learning-django
-this is a step by step series of how I leaned Django
--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for library project.
+URL configuration for blog project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
