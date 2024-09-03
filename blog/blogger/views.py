690b71a88b5ef1d778b529f115bc5fc58d6bcb0d
avict18 <avict1809@gmail.com>
2024-09-03 12:58:02 +0300

    tested rendering data with dummy data

--- blog/blogger/views.py
+++ blog/blogger/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First blog post content',
+        'date_posted': 'August 27, 2024',
+        },
+    {
+            'author': 'John Doe',
+            'title': 'Blog Post 2',
+            'content': 'Second blog post content',
+            'date_posted': 'August 28, 2024'
+            }
+]
+
 def home(request):
-    return render(request, 'blogger/home.html')
+    context = {
+        'posts':posts
+    }
+    return render(request, 'blogger/home.html',context)
